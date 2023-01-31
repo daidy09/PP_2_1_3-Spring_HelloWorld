1baadaf1e03994eeec4e10ba4eaa0dc79b8e7974
Dmitry <dziamentsyeu.dzmitry@gmail.com>
2023-01-31 10:48:10 +0300

    2_1_3 remake

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,19 +5,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = applicationContext.getBean("helloworld", HelloWorld.class);
         System.out.println(bean.getMessage());
-        HelloWorld bean1 =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 = applicationContext.getBean("helloworld", HelloWorld.class);
         System.out.println(bean1.getMessage());
         System.out.println(bean == bean1);
 
-        Cat catBean =
-                (Cat) applicationContext.getBean("cat");
+        Cat catBean = applicationContext.getBean("cat", Cat.class);
         System.out.println(catBean.getMessage());
-        Cat catBean1 =
-                (Cat) applicationContext.getBean("cat");
+        Cat catBean1 = applicationContext.getBean("cat", Cat.class);
         System.out.println(catBean1.getMessage());
         System.out.println(catBean == catBean1);
     }
